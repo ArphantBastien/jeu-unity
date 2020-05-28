b0b194fe809e66c22131913d05102906505a6ec4
ArphantBastien <arphant.bastienn@gmail.com>
2020-05-28 09:25:24 +0200

    1er commit

---
+++ Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    private Transorm target;
+    private int desPoint;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
